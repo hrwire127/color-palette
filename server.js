bbf3a5fdcd5b95bc991bbb2455f3ac2fff0e9c1c
unknown <artiomonilov2005@gmail.com>
2022-07-08 18:27:28 +0300

    7

--- server.js
+++ server.js
@@ -9,4 +9,8 @@ app.get('/*', function (req, res)
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+const dev = process.env.NODE_ENV !== 'production'
+
+const port = !dev ? process.env.PORT : 3000
+
+app.listen(port);
